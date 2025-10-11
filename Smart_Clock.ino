31fafde4e00d5ff850af39075d0bbfedb26ec555
raihan2703 <m.raihan.akbar.2703@gmail.com>
2025-10-11 11:52:27 +0700

    Smart_Clock.ino

--- Smart_Clock.ino
+++ Smart_Clock.ino
@@ -5,90 +5,97 @@
 #include <MD_Parola.h>
 #include <SPI.h>
 #include <DHT.h>
+#include <WiFiManager.h>  // Library WiFi Manager
 
-//Initialization of Internet COnnections
-const char* ssid = "RAFIF";
-const char* pass = "Rafif123";
+// ---------- Inisialisasi WiFi ----------
+WiFiManager wifiManager;
 
-//Initialization for time area
-const long utcOffsetInSeconds = 25200; //GMT+7
-
-//Initialization NTP
+// ---------- Inisialisasi NTP ----------
+const long utcOffsetInSeconds = 25200; // GMT+7
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
-//Menampilkan bulan dalam bentuk nama bulan (string)
-String bulan[13]{"", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
-
-//Initialization MAX7219
-#define HARDWARE_TYPE MD_MAX72XX :: FC16_HW
+// ---------- Inisialisasi MAX7219 ----------
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define DIN 13
 #define CS 15
 #define CLK 14
 #define MAX_DEVICES 4
 MD_Parola matrix = MD_Parola(HARDWARE_TYPE, DIN, CLK, CS, MAX_DEVICES);
 
-//Variabel untuk mengubah tanggal menuju char karena di scroll
-char arrTgl[18];
-
-//Initialization duration
-int counter = 0;
-
-//Initialization state awal
-String posisi = "jam";
-
-//Initialization for DHT Sensor
-#define DHTPIN 12
+// ---------- Inisialisasi DHT11 ----------
+#define DHTPIN 2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-//Variabel untuk mengubah float ke char
-char arrTemp[7];
+// ---------- Variabel Global ----------
+String bulan[13] = {
+  "", "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+  "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+};
 
-void setup(){
+// --------- Bikin Array yang bisa ditampilkan
+char arrTgl[30];
+char arrTemp[10];
+
+//--------- Pin LDR -------------------
+#define ldrPin A0
+
+int counter = 0;
+String posisi = "jam";
+
+void setup() {
   Serial.begin(115200);
   matrix.begin();
   matrix.displayClear();
-  WiFi.begin(ssid, pass);
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(500);
-  }
-  timeClient.begin();
   dht.begin();
+
+  // ---------- WiFi Manager ----------
+  // Jika gagal konek, ESP akan jadi Access Point (contoh SSID: SmartClock_Setup)
+  wifiManager.resetSettings();
+  wifiManager.autoConnect("SmartClock_Setup");
+  Serial.println("WiFi Connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  timeClient.begin();
 }
 
-void loop(){
-  if(posisi == "jam"){
+void loop() {
+  int ldrValue = analogRead(ldrPin);
+  int brightness = map(ldrValue, 0, 1023, 15, 0);
+  matrix.setIntensity(brightness);
+  if (posisi == "jam") {
     baca_jam();
-  }
-  else if(posisi == "tanggal"){
+  } else if (posisi == "tanggal") {
     baca_tanggal();
-  }
-  else if(posisi == "suhu"){
+  } else if (posisi == "suhu") {
     baca_suhu();
   }
 }
 
-void baca_jam(){
+void baca_jam() {
   timeClient.update();
-  String jam_lengkap = timeClient.getFormattedTime();
-  //Parsing jam, menit, detik
-  String jam, menit, detik;
-  jam = String(timeClient.getHours());
-  menit = String(timeClient.getMinutes());
-  detik = String(timeClient.getSeconds());
+  int jamInt = timeClient.getHours();
+  int menitInt = timeClient.getMinutes();
+
+  // Format dua digit
+  String jam = (jamInt < 10 ? "0" + String(jamInt) : String(jamInt));
+  String menit = (menitInt < 10 ? "0" + String(menitInt) : String(menitInt));
+
+  String jam_lengkap = jam + ":" + menit;
   Serial.println(jam_lengkap);
-  //Menampilkan ke display
+
+  // Tampilkan ke display
   matrix.setTextAlignment(PA_CENTER);
-  if((timeClient.getSeconds()) % 2 == 0){
+  if ((timeClient.getSeconds()) % 2 == 0) {
     matrix.print(jam + ":" + menit);
   } else {
     matrix.print(jam + " " + menit);
   }
-  //Untuk menentukan 20 detik ditampilkan
+
   counter++;
-  if(counter == 20){
+  if (counter >= 20) {  // 20 detik tampil
     posisi = "tanggal";
     counter = 0;
     matrix.displayClear();
@@ -96,35 +103,26 @@ void baca_jam(){
   delay(1000);
 }
 
-void baca_tanggal(){
+void baca_tanggal() {
   timeClient.update();
-  //Konfigurasi untuk mendapatkan tanggal, bulan, tahun
   time_t epochTime = timeClient.getEpochTime();
   struct tm *ptm = localtime(&epochTime);
-  //Parsing tanggal, bulan, tahun
+
   int tgl = ptm->tm_mday;
-  String sTgl = "";
-  if(tgl < 10){
-    sTgl = "0" + String(tgl);
-  } else {
-    sTgl = String(tgl);
-  }
   int bln = ptm->tm_mon + 1;
-  String array_bulan = bulan[bln];
   int thn = ptm->tm_year + 1900;
 
-  //Penggabungan dari pemarsingan diatas
-  String tanggal_sekarang = sTgl + "/" + array_bulan + "/" + String(thn);
+  String sTgl = (tgl < 10 ? "0" + String(tgl) : String(tgl));
+  String tanggal_sekarang = sTgl + " " + bulan[bln] + " " + String(thn);
+
+  tanggal_sekarang.toCharArray(arrTgl, sizeof(arrTgl));
   Serial.println(tanggal_sekarang);
 
-  //Pengkonversian dari string ke char array
-  tanggal_sekarang.toCharArray(arrTgl, 18);
-  //Tampilkan ke display
-  if(matrix.displayAnimate()){
+  if (matrix.displayAnimate()) {
     matrix.displayText(arrTgl, PA_CENTER, 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
     matrix.displayReset();
     counter++;
-    if(counter > 1){
+    if (counter >= 2) {
       posisi = "suhu";
       counter = 0;
       matrix.displayClear();
@@ -133,17 +131,27 @@ void baca_tanggal(){
   delay(100);
 }
 
-void baca_suhu(){
+void baca_suhu() {
   float t = dht.readTemperature();
-  String temp = String(t) + "C";
-  temp.toCharArray(arrTemp, 7);
+  //if (isnan(t)) {
+   // Serial.println("Gagal membaca sensor DHT!");
+    //return;
+  //}
+
+  String temp = String(t, 1) + "C";
+  temp.toCharArray(arrTemp, sizeof(arrTemp));
+
   matrix.setTextAlignment(PA_CENTER);
   matrix.print(arrTemp);
+  Serial.print("Suhu: ");
+  Serial.println(temp);
+
   counter++;
-  if(counter == 3){
+  if (counter >= 5) {  // tampil selama 5 detik
     posisi = "jam";
     counter = 0;
     matrix.displayClear();
   }
   delay(1000);
 }
+
